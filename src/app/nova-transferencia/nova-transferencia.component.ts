3c8e3eab367620c191b477b895e9511779334526
Eduardo Teixeira <dunirvana@gmail.com>
2022-06-10 13:27:23 -0300

    Ajustes no serviço para consumir uma api rest.

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,23 +1,29 @@
-import { Component } from "@angular/core";
-import { TransferenciaService } from "../services/transferencia.service";
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
-
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
   valor!: number;
   destino!: number;
 
-  constructor(private service: TransferenciaService) {}
+  constructor(private service: TransferenciaService){}
 
-  transferir(){
+  transferir() {
     console.log('Solicitada nova transferência');
+    const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
 
-    this.service.adicionar({ valor: this.valor, destino : this.destino});
+    this.service.adicionar(valorEmitir).subscribe(result => {
+      console.log(result);
+
+      this.limparCampos();
+    },
+    error => console.error(error));
   }
 
   limparCampos(){
